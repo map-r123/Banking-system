891db5c69eccb1cf7584e3bc30c8c5e13b9a1691
map-r123 <maandamap123@gmail.com>
2026-02-07 20:00:10 +0200

    Update account.cpp

--- account.cpp
+++ account.cpp
@@ -1,6 +1,7 @@
 #include "account.h"
 
 account::account() {}
+
 account::account(int a, string n, double b)
 {
     accountNumber=a;
@@ -8,17 +9,51 @@ account::account(int a, string n, double b)
     balance=b;
 }
 
-void account::deposit(double amount)
+void account::deposit()
 {
+    double amount;
+    cout << "\nDeposit Amount: ";
+    cin >> amount;
+    cout << "Deposit Successful\n";
     balance = balance + amount;
 }
 
-void account::withdraw(double amount)
+void account::withdraw()
 {
+    double amount;
+    cout << "\nWithdraw Amount: ";
+    cin >> amount;
+
+    while(amount > balance)
+    {
+        cout << "Withdraw amount is greater than current balance" << endl;
+        cout << "Withdraw Amount: ";
+        cin >> amount;
+    }
     balance = balance - amount;
+    cout << "Withdraw Successful\n";
 }
 
 double account::checkBalance()
 {
     return balance;
+}
+
+void account::createAccount()
+{
+    int acc;
+    double bal;
+    string nam;
+    cout << "***** Create Account *****" << endl;
+    cout << "\nAccount No:";
+    cin >> acc;
+    cin.ignore();
+    cout << "Account Holder Name: ";
+    getline(cin,nam);
+    cout << "Current Balance: ";
+    cin >> bal;
+
+    accountNumber=acc;
+    name=nam;
+    balance=bal;
 }
