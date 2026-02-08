84732c66bf534d44ea7037c16a9de42ef67c1c6b
map-r123 <maandamap123@gmail.com>
2026-02-08 16:18:34 +0200

    Enhance banking system with account management and login
    Refactored banking system to use a map for account management and added login functionality.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "account.h"
 #include <iomanip>
+#include <map>
 
 using namespace std;
 
@@ -8,16 +9,18 @@ int main()
 {
     int selected =0;
     string name;
-    account* newAccount= new account();
+    account newAccount;
+    map<int,account> accountList;
 
-    while (selected!=5)
+    while (selected!=6)
     {
         cout << "\n***** Simple Banking System *****" << endl;
         cout << "\n1. Create Account" << endl;
         cout << "2. Withdraw\n";
         cout << "3. Deposit\n";
         cout << "4. Check Balance\n";
-        cout << "5. Exit\n\n";
+        cout << "5. Login\n";
+        cout << "6. Exit\n\n";
         cout << "Enter your choice: ";
         cin >> selected;
         cin.ignore();
@@ -25,19 +28,40 @@ int main()
         switch (selected)
         {
         case 1:
-            newAccount->createAccount();
+            newAccount.createAccount();
+            accountList[newAccount.getAccountNumber()] = newAccount;
             break;
         case 2:
-            newAccount->withdraw();
+            newAccount.withdraw();
+            accountList[newAccount.getAccountNumber()]=newAccount;
             break;
         case 3:
-            newAccount->deposit();
+            newAccount.deposit();
+            accountList[newAccount.getAccountNumber()]=newAccount;
             break;
         case 4:
-            cout << "\nYour Balance: " << fixed << setprecision(2) << newAccount->checkBalance() ;
+            cout << "\nYour Balance: " << fixed << setprecision(2) << newAccount.checkBalance() ;
             break;
         case 5:
-            cout << "Thank you " << name <<
+            int acc,pin;
+            cout << "\n***** Login *****" << endl;
+            cout << "\nAccount No: ";
+            cin >> acc;
+            cout << "Pin: ";
+            cin >> pin;
+
+            if(accountList[acc].getPin()==pin)
+            {
+                newAccount=accountList[acc];
+                cout << "Welcome " <<newAccount.getName() << endl;
+            }
+            else
+            {
+                cout << "Login Fail (Incorret Account Number or Pin)\n";
+            }
+            break;
+        case 6:
+            cout << "Thank you " << newAccount.getName() <<
                 " for using our simple banking system";
             break;
         default:
@@ -47,9 +71,6 @@ int main()
 
     }
 
-
-    delete newAccount;
-    newAccount = nullptr;
     return 0;
 }
 
