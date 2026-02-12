428dd4db133bacf288d95e95c5fba5e502afc6ba
map-r123 <maandamap123@gmail.com>
2026-02-12 17:18:47 +0200

    Modify account class to use string for pin
    Changed pin type from int to string and added validation methods.

--- account.h
+++ account.h
@@ -11,20 +11,22 @@ private:
     int accountNumber;
     string name;
     double balance;
-    int pin;
+    string pin;
 public:
     account();
-    account(int a, string n, double b, int pin);
+    account(int a, string n, double b, string pin);
 
     void deposit();
     void withdraw();
     double checkBalance() const;
     void createAccount();
     int getAccountNumber() const;
-    int getPin() const;
+    string getPin() const;
     string getName() const;
     void saveAccounts(const map<int, account>& accList);
     void readAccounts(map<int, account>& accList);
+    bool lengthValidation(string pin);
+    bool typeValidation(string pin);
 };
 
 #endif // ACCOUNT_H
